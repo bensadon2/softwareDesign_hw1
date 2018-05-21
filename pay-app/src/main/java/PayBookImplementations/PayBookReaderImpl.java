22ebf4ed408766fdc2ec9c80e2f877d13cdc648b
SHAI-MSI\Shai Ben-Sadon <shai.bensadon@gmail.com>
2018-05-21 19:56:51 +0300

    skeleton of PayBookReader added.

---
+++ pay-app/src/main/java/PayBookImplementations/PayBookReaderImpl.java
@@ -0,0 +1,70 @@
+package PayBookImplementations;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import il.ac.technion.cs.sd.pay.app.PayBookReader;
+import persistentDatabase.PersistentDatabase;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+import static PayBookImplementations.PayBookInitializerImpl.CLIENTS;
+import static PayBookImplementations.PayBookInitializerImpl.SELLERS;
+
+public class PayBookReaderImpl implements PayBookReader {
+
+    private final PersistentDatabase dbByClients;
+    private final PersistentDatabase dbBySellers;
+
+    @Inject
+    public PayBookReaderImpl(@Named("dbByClients") PersistentDatabase dbByClients,
+                             @Named("dbBySellers") PersistentDatabase dbBySellers) {
+        this.dbByClients = dbByClients;
+        this.dbByClients.dbInstance(CLIENTS);
+        this.dbBySellers = dbBySellers;
+        this.dbBySellers.dbInstance(SELLERS);
+    }
+
+    @Override
+    public boolean paidTo(String clientId, String sellerId) {
+
+        return false;
+    }
+
+    @Override
+    public OptionalDouble getPayment(String clientId, String sellerId) {
+        return null;
+    }
+
+    @Override
+    public List<String> getBiggestSpenders() {
+        return null;
+    }
+
+    @Override
+    public List<String> getRichestSellers() {
+        return null;
+    }
+
+    @Override
+    public Optional<String> getFavoriteSeller(String clientId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<String> getBiggestClient(String sellerId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Map<String, Integer> getBiggestPaymentsToSellers() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Integer> getBiggestPaymentsFromClients() {
+        return null;
+    }
+}
